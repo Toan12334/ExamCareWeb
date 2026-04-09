ea809dfe42629685ebcb6b2b0121d3bdcf97eb24
dao xuan toan <daoxuant2@gmail.com>
2026-04-09 23:42:13 +0700

    fix api link

--- frontend/my-app/src/services/http.js
+++ frontend/my-app/src/services/http.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: window.location.origin + "/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json"
