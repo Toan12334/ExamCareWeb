7669950ae50790b263e0c0b99733cd3f988e56d0
dao xuan toan <daoxuant2@gmail.com>
2026-04-10 23:13:59 +0700

    buuton

--- frontend/my-app/src/pages/StudentExamPage.jsx
+++ frontend/my-app/src/pages/StudentExamPage.jsx
@@ -6,7 +6,7 @@ export default function StudentExamPage(){
     return(
         <div>
             <DataTable2 columns={studentExamColumns}/>
-            
+            <button className="bg-red-600">commit</button>
         </div>
     )
 }
