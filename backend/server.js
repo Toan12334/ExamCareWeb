2af5215c2cc6bb00133c9c99ebc40badec784380
dao xuan toan <daoxuant2@gmail.com>
2026-04-10 23:22:17 +0700

    test backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js"
 
 const PORT = 3000
-
+//sadadasdassadadasdas
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}`)
   console.log(`Student API: http://localhost:${PORT}/api/`)
