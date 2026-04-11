74ed09eedcec5adadf6d7b695884c62a84387a13
dao xuan toan <daoxuant2@gmail.com>
2026-04-11 09:39:34 +0700

    filter

--- frontend/my-app/src/constants/studentExam.columns.jsx
+++ frontend/my-app/src/constants/studentExam.columns.jsx
@@ -26,3 +26,39 @@ export const studentExamColumns = [
     }
 
 ]
+
+
+const statusOptions = [
+    { value: "InProgress", label: "Đang làm" },
+    { value: "Submitted", label: "Đã hoàn thành" },
+    { value: "not_started", label: "Chưa bắt đầu" }
+  ];
+  
+  const rangeScoreOptions = [
+    { value: "0-5", label: "0 - 5" },
+    { value: "5-7", label: "5 - 7" },
+    { value: "7-10", label: "7 - 10" }
+  ];
+  
+  export const buildStudentExamFilters = (
+    statusArray = statusOptions,
+    rangeScoreArray = rangeScoreOptions
+  ) => [
+    {
+      key: "search",
+      type: "input",
+      placeholder: "Tìm theo tên học sinh hoặc tên bài thi"
+    },
+    {
+      key: "status",
+      type: "select",
+      placeholder: "Trạng thái",
+      options: statusArray
+    },
+    {
+      key: "rangeScore",
+      type: "select",
+      placeholder: "Khoảng điểm",
+      options: rangeScoreArray
+    }
+  ];
--- frontend/my-app/src/pages/StudentExamPage.jsx
+++ frontend/my-app/src/pages/StudentExamPage.jsx
@@ -2,13 +2,14 @@
 import { studentExamColumns } from "../constants/studentExam.columns.jsx"
 import DataTable2 from "../components/table/DataTable2"
 import useStudentExam from "../hooks/useStudentExam.js";
+import { buildStudentExamFilters } from "../constants/studentExam.columns.jsx";
 export default function StudentExamPage(){
     const { studentExams,  pagination, loading,  error,  handleSearch,  handleFilter, handlePagination, handleSort, resetFilters} = useStudentExam();
 
     return(
         <div>
             <DataTable2 pagination={pagination} loading={loading} error={error} onSearch={handleSearch} onFilter={handleFilter} onPagination={handlePagination} onSort={handleSort} onResetFilters={resetFilters}
-            columns={studentExamColumns} data={studentExams} title="Quản lý học sinh đang thi"/>
+            columns={studentExamColumns} data={studentExams} title="Quản lý học sinh đang thi" filters={buildStudentExamFilters}/>
         </div>
     )
 }
