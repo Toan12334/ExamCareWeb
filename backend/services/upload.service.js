f953667da8104fe510df538704483db7b63915aa
dao xuan toan <daoxuant2@gmail.com>
2026-04-20 15:57:04 +0700

    fff

--- backend/services/upload.service.js
+++ backend/services/upload.service.js
@@ -2,7 +2,7 @@ import cloudinary from "../config/cloudinary.js";
 import fs from "fs";
 
 class cloudinaryService {
-      async uploadImage  (filePath)  {
+    async uploadImage(filePath) {
         try {
             const result = await cloudinary.uploader.upload(filePath, {
                 folder: "questions",
@@ -21,7 +21,7 @@ class cloudinaryService {
             // Sử dụng Promise.all để upload tất cả ảnh song song
             const uploadPromises = filePaths.map(path => this.uploadImage(path));
             const secureUrls = await Promise.all(uploadPromises);
-            
+
             return secureUrls; // Trả về mảng các link ảnh: ["url1", "url2", ...]
         } catch (error) {
             throw new Error(`Multiple Upload Error: ${error.message}`);
@@ -34,10 +34,12 @@ class cloudinaryService {
             const result = await cloudinary.uploader.upload(filePath, {
                 resource_type: "raw", // bắt buộc cho file Word
                 folder: "documents",
+                use_filename: true,    // Sử dụng tên file gốc của bạn
+                unique_filename: true, // Đảm bảo tên file không bị trùng
             });
-    
+
             fs.unlinkSync(filePath);
-    
+
             return result.secure_url;
         } catch (error) {
             throw new Error(error.message);
@@ -45,4 +47,4 @@ class cloudinaryService {
     }
 }
 
-export default new  cloudinaryService
+export default new cloudinaryService
