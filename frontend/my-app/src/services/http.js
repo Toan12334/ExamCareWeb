e893272e9e8aa6b8720993ed29b2ae3687dc0fae
dao xuan toan <daoxuant2@gmail.com>
2026-04-20 15:30:52 +0700

    fox

--- frontend/my-app/src/services/http.js
+++ frontend/my-app/src/services/http.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
 // Nó sẽ ưu tiên lấy link từ .env.local trước, nếu không có mới lấy origin
-const base = import.meta.env.VITE_API_BASE_URL || window.location.origin;
-console.log(">>> CHECK ENV:", import.meta.env.VITE_API_BASE_URL);
+const base = import.meta.env.VITE_API_BASE_URL;
+
 const axiosClient = axios.create({
-  baseURL: base + "/api", 
+  baseURL: base ? base + "/api" : window.location.origin + "/api",
   timeout: 60000,
   headers: {
     "Content-Type": "application/json"
   }
-})
+});
 
 export default axiosClient
