450cd58407a36b574e32a0a9cf941ff2dd554eb7
dao xuan toan <daoxuant2@gmail.com>
2026-04-24 02:00:15 +0700

    build

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import app from "./app.js"
 
-const PORT = 3000
-//sadadasdassadadasdas
-app.listen(PORT, () => {
-  console.log(`Server running at: http://localhost:${PORT}`)
-  console.log(`Student API: http://localhost:${PORT}/api/`)
+// 1. Render sẽ truyền vào biến process.env.PORT. 
+// Nếu chạy dưới local máy bạn (không có biến này), nó sẽ tự động dùng cổng 3000.
+const PORT = process.env.PORT || 3000;
+
+// 2. BẮT BUỘC phải thêm '0.0.0.0' để Docker mở kết nối ra bên ngoài
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port: ${PORT}`)
+  console.log(`Student API is ready on port ${PORT}`)
 })
