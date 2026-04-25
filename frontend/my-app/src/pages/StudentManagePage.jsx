1eebcf0d39c31c4f0d35e47c051e7b438b976dd1
dao xuan toan <daoxuant2@gmail.com>
2026-04-25 20:10:46 +0700

    toast delete user

--- frontend/my-app/src/pages/StudentManagePage.jsx
+++ frontend/my-app/src/pages/StudentManagePage.jsx
@@ -6,9 +6,10 @@ import { Pencil, Trash2 } from "lucide-react"
 import StudentForm from "../components/form/StudentForm.jsx"
 import StudentFilter from "../components/search/StudentFilter.jsx"
 import DeleteConfirmation from "../components/modal/DeleteConfirmation.jsx"
+import Toast from "../utils/toast.js"
 export function StudentManagePage() {
 
-    const { students, createStudent, getStudentId, updateStudentId,deleteStudent,searchStudent,fetchStudents } = useStudent()
+    const { students, createStudent, getStudentId, updateStudentId, deleteStudent, searchStudent, fetchStudents } = useStudent()
 
     const [openForm, setOpenForm] = useState(false)
     const [editingStudent, setEditingStudent] = useState(null)
@@ -20,10 +21,10 @@ export function StudentManagePage() {
         } else {
             await createStudent(data)
         }
-    
+
         setEditingStudent(null)
         setOpenForm(false)
-    }   
+    }
 
     const handlegetStudentId = async (id) => {
         const result = await getStudentId(id)
@@ -36,8 +37,9 @@ export function StudentManagePage() {
         setEditingStudent(null)
     }
 
-    const handleDeleteStudent=async(id)=>{
+    const handleDeleteStudent = async (id) => {
         await deleteStudent(id)
+        Toast.success("Xóa học sinh thành công")
     }
 
     const columns = [
@@ -49,15 +51,15 @@ export function StudentManagePage() {
 
     return (
         <div>
-            <StudentFilter onSearch={searchStudent} onReset={fetchStudents}/>
-            
+            <StudentFilter onSearch={searchStudent} onReset={fetchStudents} />
+
             <DataTable
                 columns={columns}
                 data={students}
                 onAdd={() => setOpenForm(true)}
                 renderActions={(row) => (
                     <div className="flex justify-center gap-2">
-                        <DeleteConfirmation onDelete={()=>handleDeleteStudent(row.StudentId)}>
+                        <DeleteConfirmation onDelete={() => handleDeleteStudent(row.StudentId)}>
                             <Trash2 size={18} />
                         </DeleteConfirmation>
 
